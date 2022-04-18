9b3da669e1b5de3b777afff746dee84f3fbe0210
vikaskumar075 <vikaskumar638727@gmail.com>
2022-04-18 15:41:25 -0700

    My University website

---
+++ formholder.php
@@ -0,0 +1,24 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_form = 'info@yourwebsite.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n".
+              "User Email: $visitors_email.\n".
+              "Subject: $subject.\n".
+              "User Message: $message.\n";
+
+$to = 'vikaskumar638727@gmail.com';
+
+$headers = "From: $email_from\r\n";
+
+$headers = "Reply-To: $visitors_email\r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+header("Location: contact.html");
+?>
